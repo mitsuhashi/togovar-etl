9d4b3de2f8d4a8e4ea75a44c585fce2d58956c5d
Nobutaka Mitsuhashi <mitsuhashi@dbcls.rois.ac.jp>
2024-10-21 17:34:53 +0900

    update transanno at 2024.2

--- liftover/transanno.sh
+++ liftover/transanno.sh
@@ -8,5 +8,8 @@ OUTDIR=$DATADIR/liftover_grch37_38/
 
 #cwltool --debug --cachedir cache --outdir $OUTDIR/jga_snp --log-dir logs transanno.cwl input_jga_snp.yml
 
-toil-cwl-runner --logDebug --workDir ./toil_work --outdir $OUTDIR/bbj_riken --logFile toil_logs/bbj_riken.log transanno.cwl input_bbj_riken_freq.yml
+#toil-cwl-runner --logDebug --workDir ./toil_work --outdir $OUTDIR/bbj_riken --logFile toil_logs/bbj_riken.log transanno.cwl input_bbj_riken_freq.yml
+#toil-cwl-runner --logDebug --workDir ./toil_work --outdir $OUTDIR/mgend --logFile toil_logs/mgend.log transanno.cwl input_grch37_mgend.yml
+
+cwltool --debug --cachedir cache --outdir $OUTDIR/mgend --log-dir logs transanno.cwl input_grch37_mgend.yml
 
